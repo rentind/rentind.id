bcb0fdb2062af90ab6ddf5cc3d2150528212fe12
Muh Suryadi Triputra Rahman <msuryaditriputra@gmail.com>
2023-06-17 23:20:26 +0800

    add: fetch kec

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,3 +8,24 @@ fetch(`https://www.emsifa.com/api-wilayah-indonesia/api/regencies/73.json`)
       ({ id, name }) => `<option value="${id}">${name}</option>`
     );
   });
+
+/* ==================== FETCH DATA KECAMATAN ===================== */
+const selectKec = document.getElementById("kecamatan");
+
+selectKota.addEventListener("change", function () {
+  const idKec = this.value;
+
+  selectKec.removeAttribute("disabled");
+
+  fetch(
+    `https://www.emsifa.com/api-wilayah-indonesia/api/districts/${idKec}.json`
+  )
+    .then((response) => response.json())
+    .then((districts) => {
+      selectKec.innerHTML = `<option value="" hidden>Pilih Kecamatan</option>`;
+
+      selectKec.innerHTML += districts.map(
+        ({ id, name }) => `<option value="${id}">${name}</option>`
+      );
+    });
+});
